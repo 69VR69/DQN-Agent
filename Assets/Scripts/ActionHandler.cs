930bf3aa527a2757e31c90816c3ed2c8b5d8ef37
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-10-15 21:11:20 -0700

    Add move, rotate and jump methods (to test)

--- Assets/Scripts/ActionHandler.cs
+++ Assets/Scripts/ActionHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 
 using Assets.Scripts;
 
@@ -17,22 +18,22 @@ public class ActionHandler : MonoBehaviour
     [SerializeField] private float _moveSpeed = 10f;
     [SerializeField] private float _rotationSpeed = 10f;
 
-    public void Move(Vector2 direction)
+    public void Move(Vector3 direction)
     {
         Debug.Log($"Move to {direction}");
-        //_agent.Move(direction * _moveSpeed);
+        _agent.Move(direction * _moveSpeed);
     }
 
     public void Jump()
     {
         Debug.Log("Jump");
-        //_agent.Jump(_jumpForce);
+        _agent.Jump(_jumpForce);
     }
 
-    public void Rotate(Vector2 direction)
+    public void Rotate(Quaternion direction)
     {
         Debug.Log($"Rotate to {direction}");
-        //_agent.Rotate(direction * _rotationSpeed);
+        _agent.Rotate(direction * _rotationSpeed);
     }
 
     public void MakeAction(AgentAction action)
@@ -40,28 +41,28 @@ public class ActionHandler : MonoBehaviour
         switch (action)
         {
             case AgentAction.MOVE_UP:
-                Move(Vector2.up);
+                Move(Vector3.up);
                 break;
             case AgentAction.MOVE_RiGHT:
-                Move(Vector2.right);
+                Move(Vector3.right);
                 break;
             case AgentAction.MOVE_DOWN:
-                Move(Vector2.down);
+                Move(Vector3.down);
                 break;
             case AgentAction.MOVE_LEFT:
-                Move(Vector2.left);
+                Move(Vector3.left);
                 break;
             case AgentAction.TURN_RIGHT:
-                Rotate(Vector2.right);
+                Rotate(new Quaternion(Vector3.Zero, 1));
                 break;
             case AgentAction.TURN_LEFT:
-                Rotate(Vector2.left);
+                Rotate(new Quaternion(Vector3.Zero, -1));
                 break;
             case AgentAction.JUMP:
                 Jump();
                 break;
-                case AgentAction.WAIT:
-                    break;
+            case AgentAction.WAIT:
+                break;
             default:
                 break;
         }
--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,9 +1,28 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.PeerToPeer.Collaboration;
+using System.Numerics;
 
 using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
     public float Reward { get; internal set; }
+    private RigidBody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<RigidBody>();
+    }
+
+    public void Jump(float jumpForce) =>
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+    public void Move(Vector3 movement) =>
+        rb.velocity = movement * Time.fixedDeltaTime;
+
+
+    public void Rotate(Quaternion rotation)
+        => rb.rotation += rotation;
 }
